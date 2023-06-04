2625b0d566b60634dc4656882e68ba921756b38b
aadice02 <aadice02@gmail.com>
2023-06-04 09:03:07 +0000

    three tier app code

---
+++ modules/resourcegroup/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "ODL-azure-971715" {
+  name     = var.name
+  location = var.location
+}
---
+++ modules/resourcegroup/output.tf
@@ -0,0 +1,9 @@
+output "resource_group_name" {
+    value = azurerm_resource_group.ODL-azure-971715.name
+    description = "Name of the resource group."
+}
+
+output "location_id" {
+    value = azurerm_resource_group.ODL-azure-971715.location
+    description = "Location id of the resource group"
+}
