75bf60c1087ac25abb1a583c520406dd4c736eaa
aadice02 <aadice02@gmail.com>
2023-06-04 19:02:57 +0000

    metadata script added

---
+++ metadata.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install Azure CLI (uncomment the line below if Azure CLI is not installed)
+# az --version || curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+VMNAME=$3
+RESOURCEGROUPNAME=$2
+# Get the instance metadata
+metadata=$(az vm show -d --name $VMNAME  --resource-group $RESOURCEGROUPNAME --output json)
+
+# Function to retrieve value for a specific data key
+get_value() {
+key=$1
+value=$(echo "$metadata" | jq -r ".$key")
+echo "$value"
+}
+
+# Output the metadata in JSON format
+#echo "$metadata" | jq .
+
+# Retrieve a specific data key value if provided
+if [ -n "$1" ]; then
+key_value=$(get_value "$1")
+if [ "$key_value" == "null" ]; then
+echo "Data key '$1' not found."
+else
+echo "Value for data key '$1': $key_value"
+fi
+fi
+# Output the metadata in JSON format
+#echo $metadata | jq .
