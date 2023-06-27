c9d22337b8dc673304b44fabf513bc7a0de74352
Abhishek Gupta <42316673+aadice02@users.noreply.github.com>
2023-06-28 02:16:33 +0530

    Update vars.tf

--- modules/compute/vars.tf
+++ modules/compute/vars.tf
@@ -4,7 +4,12 @@ variable "web_subnet_id" {}
 variable "app_subnet_id" {}
 variable "web_host_name"{}
 variable "web_username" {}
-variable "web_os_password" {}
+#variable "web_os_password" {}
 variable "app_host_name"{}
 variable "app_username" {}
-variable "app_os_password" {}
+#variable "app_os_password" {}
+variable "password_length" {
+description = "Length of the random password"
+type = number
+default = 16
+}
