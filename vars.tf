e953183ec7db2883b43ba910a949c5b0dd3f26df
Abhishek Gupta <42316673+aadice02@users.noreply.github.com>
2023-06-28 02:18:19 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -6,10 +6,10 @@ variable "appsubnetcidr" {}
 variable "dbsubnetcidr" {}
 variable "web_host_name"{}
 variable "web_username" {}
-variable "web_os_password" {}
+# variable "web_os_password" {}
 variable "app_host_name"{}
 variable "app_username" {}
-variable "app_os_password" {}
+# variable "app_os_password" {}
 variable "primary_database" {}
 variable "primary_database_admin" {}
 variable "primary_database_password" {}
